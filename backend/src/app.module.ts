fa81c66f62c2ae4b3d90e71edd9ce86ca3bddbea
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-06 20:04:27 -0300

    feat: configure Mongoose for MongoDB connection in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://admin:password@localhost:27017/nest?authSource=admin',
+    ),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
