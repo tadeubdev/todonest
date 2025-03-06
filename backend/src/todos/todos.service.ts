d0d1e7d3ca231b0e69034a52ca608bfc8ebd58eb
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-06 20:53:51 -0300

    feat: add pagination to findAll method in TodosService

--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -13,8 +13,14 @@ export class TodosService {
     return await this.catModel.create(createTodoDto);
   }
 
-  findAll() {
-    return this.catModel.find();
+  findAll(page = 1, limit = 10) {
+    const skip = (page - 1) * limit;
+    return this.catModel
+      .find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .exec();
   }
 
   findOne(_id: string) {
