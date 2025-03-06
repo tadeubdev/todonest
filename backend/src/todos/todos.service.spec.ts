0f6336d33f0944262b70e6bbc3a64514e47d9e53
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-06 20:16:17 -0300

    test: add unit test for creating a todo in TodosService

--- backend/src/todos/todos.service.spec.ts
+++ backend/src/todos/todos.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosService } from './todos.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Todo } from './schemas/todo.schema';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 describe('TodosService', () => {
   let service: TodosService;
 
+  const mockTodoModel = {
+    exists: jest.fn(),
+    create: jest.fn().mockReturnThis(),
+    find: jest.fn().mockReturnThis(),
+    skip: jest.fn().mockReturnThis(),
+    limit: jest.fn().mockReturnValue([]),
+    findOne: jest.fn(),
+    findOneAndUpdate: jest.fn(),
+    findOneAndDelete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodosService],
+      providers: [
+        TodosService,
+        {
+          provide: getModelToken(Todo.name),
+          useValue: mockTodoModel,
+        },
+      ],
     }).compile();
 
     service = module.get<TodosService>(TodosService);
@@ -14,5 +34,22 @@ describe('TodosService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a todo', () => {
+    const createTodoDto: CreateTodoDto = {
+      title: 'Test Todo',
+      description: 'Test Description',
+      completed: false,
+    };
+    const createdTodo = {
+      _id: '1',
+      ...createTodoDto,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    mockTodoModel.exists.mockResolvedValue(false);
+    mockTodoModel.create.mockResolvedValue(createdTodo);
+    void expect(service.create(createTodoDto)).resolves.toEqual(createdTodo);
   });
 });
