01d096ef8c9a4a6f46cdc6b5c05b4beaf5b221f3
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-06 20:04:17 -0300

    feat: integrate mongoose model for Todo in TodosService

--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Todo } from './schemas/todo.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TodosService {
+  constructor(@InjectModel(Todo.name) private catModel: Model<Todo>) {}
+
   create(createTodoDto: CreateTodoDto) {
     return 'This action adds a new todo';
   }
@@ -12,15 +17,15 @@ export class TodosService {
     return `This action returns all todos`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} todo`;
   }
 
-  update(id: number, updateTodoDto: UpdateTodoDto) {
+  update(id: string, updateTodoDto: UpdateTodoDto) {
     return `This action updates a #${id} todo`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} todo`;
   }
 }
