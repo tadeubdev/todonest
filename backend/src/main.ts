3cc2f5ad7e5ee6c759e069d8036cf9ade941907e
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-06 20:43:13 -0300

    feat: configure main with api defaults

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
