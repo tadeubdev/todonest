96ac49ac80f873899993d90a1f343f6f6bb1763f
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-07 12:51:02 -0300

    refactors: refactors TodoItem to use  TodoItemEnum and simplifies the code

--- frontend/src/components/Todos/Molecules/TodoItem.vue
+++ frontend/src/components/Todos/Molecules/TodoItem.vue
@@ -1,33 +1,40 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch, type Ref } from 'vue';
 import TodoCheckbox from '../Atoms/TodoCheckbox.vue';
 import TodoButton from '../Atoms/TodoButton.vue';
+import type { TodoItemEnum } from '../../../enums/Todos/TodoItemEnum';
 
 const emits = defineEmits(['removeTodo', 'toggleTodo']);
 
 const props = defineProps({
   todo: {
-    type: Object,
+    type: Object as () => TodoItemEnum,
+    default: () => ({
+      id: '0',
+      title: '',
+      description: '',
+      completed: false,
+      loading: false,
+    }),
     required: true,
   },
 });
 
-const removeTodo = (id: number) => {
-  emits('removeTodo', id);
-};
+const todo: Ref<TodoItemEnum> = ref(props.todo);
+const isCompleted: Ref<boolean> = ref(todo.value.completed);
 
-const todo = ref(props.todo);
+const removeTodo = () => {
+  emits('removeTodo', todo.value.id);
+};
 
 const toggleTodo = () => {
-  isCompleted.value = !todo.value.completed;
-  todo.value.completed = todo.value.completed;
-  emits('toggleTodo', isCompleted.value);
+  emits('toggleTodo', todo.value.id);
 };
 
-const isCompleted = ref(todo.value.completed);
-watch(() => todo.value.completed, (newValue) => {
-  isCompleted.value = newValue;
-}, { immediate: true });
+watch(() => todo.value, (newValue) => {
+  isCompleted.value = newValue.completed;
+  todo.value = newValue;
+}, { immediate: true, deep: true });
 </script>
 
 <template>
@@ -59,7 +66,7 @@ watch(() => todo.value.completed, (newValue) => {
     </div>
     <TodoButton
       class="p-2 focus:outline-none text-gray-600 cursor-pointer hover:text-red-500"
-      @click.stop="removeTodo(todo.id)"
+      @click.stop="removeTodo"
     >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
