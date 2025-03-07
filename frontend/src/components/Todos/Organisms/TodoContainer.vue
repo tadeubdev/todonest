a8b222b0f7bafbf71f2fa34c3cc8427048ec9ed7
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-06 22:35:48 -0300

    feat: creates the component TodoContainer to group the list items

---
+++ frontend/src/components/Todos/Organisms/TodoContainer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center bg-white md:w-lg md:size-150 w-86 min-h-5/8 rounded-lg border-1 border-blue-300"
+  >
+    <slot />
+  </div>
+</template>
