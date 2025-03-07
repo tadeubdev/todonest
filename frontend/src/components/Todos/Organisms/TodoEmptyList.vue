f7bafdecf667d7b97a50f5fc09fff68a153fa05a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-07 12:22:39 -0300

    feat: creates TodoEmptyList component to display when no todos are present

---
+++ frontend/src/components/Todos/Organisms/TodoEmptyList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="w-full h-full flex items-center justify-center"
+  >
+    <div class="flex flex-col items-center justify-center">
+      <p class="text-gray-400 mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-30">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
+        </svg>
+      </p>
+      <h1 class="text-2xl text-gray-400">Nenhuma tarefa ainda</h1>
+      <p class="text-md text-gray-400">Adicione uma nova tarefa para começar.</p>
+    </div>
+  </div>
+</template>
