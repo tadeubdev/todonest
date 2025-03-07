e193860f3f975e9ecd6b67bf51871733d78d7b7c
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-07 13:14:38 -0300

    feat: adds support to spinner to button with loading

--- frontend/src/components/Todos/Atoms/TodoButton.vue
+++ frontend/src/components/Todos/Atoms/TodoButton.vue
@@ -10,10 +10,16 @@ const props = defineProps({
     type: String,
     default: '',
   },
+  spinner: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const emits = defineEmits(['on:click']);
 const text = ref(props.text);
+const spinner = ref(props.spinner);
+
 const buttonClass = ref(
   props.class || 'w-full py-2 px-3 border-1 border-blue-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent'
 );
@@ -25,15 +31,36 @@ const onClick = () => {
 watch(() => props.text, (newValue) => {
   text.value = newValue;
 }, { immediate: true });
+
+watch(() => props.spinner, (newValue) => {
+  spinner.value = newValue;
+}, { immediate: true });
 </script>
 
 <template>
   <button
     type="button"
     :value="text"
-    :class="buttonClass"
+    :class="[
+      ...[buttonClass],
+      spinner ? 'spinner' : '',
+    ]"
     @click="onClick"
   >
     <slot />
   </button>
 </template>
+
+<style>
+.spinner svg {
+  animation: spin 1s linear infinite;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
