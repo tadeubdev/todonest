786e1997d8ecf5407b43afdfcd6a17c5f1dca229
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-06 22:35:16 -0300

    feat: creates the component TodoList to manage the tasks list

---
+++ frontend/src/components/Todos/Organisms/TodoList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import TodoItem from '../Molecules/TodoItem.vue';
+
+const props = defineProps({
+  todos: {
+    type: Array,
+    required: true,
+  },
+});
+
+const todos = ref(props.todos);
+
+const emits = defineEmits(['removeTodo', 'toggleTodo']);
+
+const removeTodo = (id: number) => {
+  emits('removeTodo', id);
+};
+
+const toggleTodo = (id: number) => {
+  emits('toggleTodo', id);
+};
+
+watch(() => props.todos, (newTodos) => {
+  todos.value = newTodos;
+}, { immediate: true });
+</script>
+
+<template>
+  <div class="w-full">
+    <TodoItem
+      v-for="todo in todos"
+      :key="`todo-${todo.id}`"
+      :todo="todo"
+      @removeTodo="removeTodo(todo.id)"
+      @toggleTodo="toggleTodo(todo.id)"
+    />
+  </div>
+</template>
