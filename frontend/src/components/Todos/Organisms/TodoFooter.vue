cfa6d73ee1ff7be4c876fce4ad5310ff23549f5f
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-07 12:29:13 -0300

    feat: adds TodoButton component to TodoFooter to send new tasks

--- frontend/src/components/Todos/Organisms/TodoFooter.vue
+++ frontend/src/components/Todos/Organisms/TodoFooter.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import TodoInput from '../Atoms/TodoInput.vue';
+import TodoButton from '../Atoms/TodoButton.vue';
 
 const emits = defineEmits(['addTodo']);
 
@@ -9,10 +10,17 @@ const text = ref('');
 
 <template>
   <div
-    class="border-t-1 border-blue-300 flex items-center justify-between p-4 md:px-8 bg-white rounded-b-lg"
+    class="border-t-1 border-blue-300 flex gap-4 items-center justify-between p-4 md:px-8 bg-white rounded-b-lg"
   >
     <TodoInput
       placeholder="Adicione uma nova tarefa..."
     />
+    <TodoButton
+      class="p-3 rounded-lg bg-blue-600 border-0 focus:outline-none text-white cursor-pointer hover:bg-blue-700"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+      </svg>
+    </TodoButton>
   </div>
 </template>
