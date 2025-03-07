3d5ae0157e10b712b0027b49bfdc88f35d54ce20
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-07 12:17:13 -0300

    style: adjusts the min and max height of TodoContainer

--- frontend/src/components/Todos/Organisms/TodoContainer.vue
+++ frontend/src/components/Todos/Organisms/TodoContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-white md:w-lg md:size-150 w-86 min-h-5/8 rounded-lg border-1 border-blue-300 grid grid-rows-[1fr_100px]"
+    class="bg-white min-h-4/8 max-h-[60vh] md:w-lg md:size-150 w-86 md:min-h-5/8 rounded-lg border-1 border-blue-300 grid grid-rows-[1fr_100px]"
   >
     <slot />
   </div>
