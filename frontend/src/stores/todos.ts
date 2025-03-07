e4b9201ab034715193cd34c9aae4d8a239064f13
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2025-03-07 13:46:01 -0300

    feat: fix the toggleTodo method to correctly sets the tasks as loading

--- frontend/src/stores/todos.ts
+++ frontend/src/stores/todos.ts
@@ -63,7 +63,7 @@ export const useTodosStore = defineStore('todos', {
             reject(error);
           })
           .finally(() => {
-            this.loading = false;
+            this.todos[todoIndex].loading = false;
           });
       });
     },
